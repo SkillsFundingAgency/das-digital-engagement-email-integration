bca302b72cc170780204bd845439fac907943850
AliShah2 <kishwer01@gmail.com>
2026-03-28 11:16:17 +0000

    Added more tests for performacne data importer

--- src/DAS.DigitalEngagement.EmailIntegration.UnitTests/Functions/PerformanceDataImporterTests.cs
+++ src/DAS.DigitalEngagement.EmailIntegration.UnitTests/Functions/PerformanceDataImporterTests.cs
@@ -3,6 +3,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 using Moq;
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DAS.DigitalEngagement.EmailIntegration.UnitTests.Functions;
@@ -48,34 +49,71 @@ public class PerformanceDataImporterTests
     }
 
     [Test]
-    public async Task Run_LogsError_WhenExceptionThrown()
+    public async Task Run_LogsError_WhenHandlerThrowsException()
     {
         // Arrange
         var loggerMock = new Mock<ILogger<PerformanceDataImporter>>();
         var importHandlerMock = new Mock<IImportCampaignPerformanceHandler>();
+        importHandlerMock
+            .Setup(x => x.Handle(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Handler failure"));
         var sut = new PerformanceDataImporter(importHandlerMock.Object, loggerMock.Object);
         var timerInfo = new TimerInfo();
 
-        // Make the logger throw when the "ran successfully" information message is logged
-        loggerMock.Setup(x => x.Log(
-            LogLevel.Information,
-            It.IsAny<EventId>(),
-            It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Performance data import ran successfully.")),
-            null,
-            It.IsAny<Func<It.IsAnyType, Exception, string>>()))
-            .Throws(new Exception("Simulated exception during import"));
-
         // Act
         await sut.Run(timerInfo);
+
         // Assert
         loggerMock.Verify(
             x => x.Log(
                 LogLevel.Error,
                 It.IsAny<EventId>(),
-                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Error importing performance data:")),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Error importing performance data")),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+    }
+
+    [Test]
+    public async Task Run_DoesNotThrow_WhenHandlerThrowsException()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<PerformanceDataImporter>>();
+        var importHandlerMock = new Mock<IImportCampaignPerformanceHandler>();
+        importHandlerMock
+            .Setup(x => x.Handle(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Handler failure"));
+        var sut = new PerformanceDataImporter(importHandlerMock.Object, loggerMock.Object);
+        var timerInfo = new TimerInfo();
+
+        // Act & Assert - should not throw
+        Assert.DoesNotThrowAsync(async () => await sut.Run(timerInfo));
+    }
+
+    [Test]
+    public async Task Run_DoesNotLogSuccess_WhenHandlerThrowsException()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<PerformanceDataImporter>>();
+        var importHandlerMock = new Mock<IImportCampaignPerformanceHandler>();
+        importHandlerMock
+            .Setup(x => x.Handle(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Handler failure"));
+        var sut = new PerformanceDataImporter(importHandlerMock.Object, loggerMock.Object);
+        var timerInfo = new TimerInfo();
+
+        // Act
+        await sut.Run(timerInfo);
+
+        // Assert
+        loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Performance data import ran successfully")),
                 null,
                 It.IsAny<Func<It.IsAnyType, Exception, string>>()),
-            Times.Never); // Adjust this to Times.Once if you want to simulate an exception and verify the error logging.
+            Times.Never);
     }
 
     [Test]
@@ -97,6 +135,94 @@ public class PerformanceDataImporterTests
                 null,
                 It.IsAny<Func<It.IsAnyType, Exception, string>>()),
             Times.Once);
+    }
+
+    [Test]
+    public async Task Run_CallsHandlerHandle()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<PerformanceDataImporter>>();
+        var importHandlerMock = new Mock<IImportCampaignPerformanceHandler>();
+        var sut = new PerformanceDataImporter(importHandlerMock.Object, loggerMock.Object);
+        var timerInfo = new TimerInfo();
+
+        // Act
+        await sut.Run(timerInfo);
+
+        // Assert
+        importHandlerMock.Verify(x => x.Handle(It.IsAny<CancellationToken>()), Times.Once);
+    }
+
+    [Test]
+    public async Task Run_LogsElapsedTime_Always()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<PerformanceDataImporter>>();
+        var importHandlerMock = new Mock<IImportCampaignPerformanceHandler>();
+        var sut = new PerformanceDataImporter(importHandlerMock.Object, loggerMock.Object);
+        var timerInfo = new TimerInfo();
+
+        // Act
+        await sut.Run(timerInfo);
+
+        // Assert
+        loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Performance data import finished in")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+    }
+
+    [Test]
+    public async Task Run_LogsElapsedTime_EvenWhenHandlerThrows()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<PerformanceDataImporter>>();
+        var importHandlerMock = new Mock<IImportCampaignPerformanceHandler>();
+        importHandlerMock
+            .Setup(x => x.Handle(It.IsAny<CancellationToken>()))
+            .ThrowsAsync(new Exception("Handler failure"));
+        var sut = new PerformanceDataImporter(importHandlerMock.Object, loggerMock.Object);
+        var timerInfo = new TimerInfo();
+
+        // Act
+        await sut.Run(timerInfo);
+
+        // Assert - finally block should still log elapsed time
+        loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Information,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Performance data import finished in")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Once);
+    }
+
+    [Test]
+    public async Task Run_DoesNotLogOverdueWarning_WhenTimerIsNotPastDue()
+    {
+        // Arrange
+        var loggerMock = new Mock<ILogger<PerformanceDataImporter>>();
+        var importHandlerMock = new Mock<IImportCampaignPerformanceHandler>();
+        var sut = new PerformanceDataImporter(importHandlerMock.Object, loggerMock.Object);
+        var timerInfo = new TimerInfo { IsPastDue = false };
+
+        // Act
+        await sut.Run(timerInfo);
+
+        // Assert
+        loggerMock.Verify(
+            x => x.Log(
+                LogLevel.Warning,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString().Contains("Timer schedule status: overdue")),
+                null,
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()),
+            Times.Never);
     }
 
 }
