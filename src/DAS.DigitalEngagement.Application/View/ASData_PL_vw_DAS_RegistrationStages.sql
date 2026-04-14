ffed6920273b3b7ab2cb59bc1858236c8aaaee3a
VijayMartinEdu <84141454+VijayMartinEdu@users.noreply.github.com>
2026-04-14 09:46:50 +0100

    ESI-239-Employer account Id Data field (#28)
    * Employer account Id Datafield
    
    * Char limit is removed
    
    * Refactored and corrected
    
    * ignored sonar validation
    
    * sonar ingore

--- src/DAS.DigitalEngagement.Application/View/ASData_PL_vw_DAS_RegistrationStages.sql
+++ src/DAS.DigitalEngagement.Application/View/ASData_PL_vw_DAS_RegistrationStages.sql
@@ -1,14 +1,11 @@
-﻿/****** Object:  View [ASData_PL].[vw_DAS_RegistrationStages]    Script Date: 07/04/2026 01:12:32 ******/
-DROP VIEW [ASData_PL].[vw_DAS_RegistrationStages]
+﻿DROP VIEW [ASData_PL].[vw_DAS_RegistrationStages]
 GO
 
-/****** Object:  View [ASData_PL].[vw_DAS_RegistrationStages]    Script Date: 07/04/2026 01:12:32 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
-
 
 
 CREATE VIEW [ASData_PL].[vw_DAS_RegistrationStages]
@@ -32,34 +29,34 @@ cte_Agreement AS (
     JOIN ASData_PL.Acc_AccountLegalEntity ale ON ale.AccountId = a.Id
     JOIN ASData_PL.Acc_EmployerAgreement ea ON ea.AccountLegalEntityId = ale.Id
 ),
+-- sonar-ignore-start
 cte_Stages AS (
     SELECT
         a.Id AS EmployerAccountId,
         a.Name AS EmployerName,
         u.Email AS UserEmail,
 
-        CASE WHEN u.Email IS NOT NULL THEN 'Y' ELSE 'N' END AS Stage1a,
-        CASE WHEN aur.Role IS NOT NULL THEN 'Y' ELSE 'N' END AS Stage1b,
-        CASE WHEN paye.PayeAdded IS NOT NULL THEN 'Y' ELSE 'N' END AS Stage2,
+        CASE WHEN u.Email IS NOT NULL THEN 'true' ELSE 'false' END AS Stage1a,
+        CASE WHEN aur.Role IS NOT NULL THEN 'true' ELSE 'false' END AS Stage1b,
+        CASE WHEN paye.PayeAdded IS NOT NULL THEN 'true' ELSE 'false' END AS Stage2,
 
         CASE 
             WHEN a.NameConfirmed = 1 
              AND a.ApprenticeshipEmployerType <> 2
              AND a.Name <> 'MY ACCOUNT'
-            THEN 'Y'
-            ELSE 'N'
+            THEN 'true'
+            ELSE 'false'
         END AS Stage3,
 
-        CASE WHEN ag.SignedAgreementId IS NOT NULL THEN 'Y' ELSE 'N' END AS Stage4a,
-        CASE WHEN ag.Acknowledged = 1 AND ag.SignedAgreementId IS NULL THEN 'Y' ELSE 'N' END AS Stage4b,
+        CASE WHEN ag.SignedAgreementId IS NOT NULL THEN 'true' ELSE 'false' END AS Stage4a,
+        CASE WHEN ag.Acknowledged = 1 AND ag.SignedAgreementId IS NULL THEN 'true' ELSE 'false' END AS Stage4b,
 
-        CASE WHEN a.AddTrainingProviderAcknowledged = 1 THEN 'Y' ELSE 'N' END AS Stage5a,
+        CASE WHEN a.AddTrainingProviderAcknowledged = 1 THEN 'true' ELSE 'false' END AS Stage5a,
         CASE 
             WHEN prov.AccountId IS NULL 
              AND a.AddTrainingProviderAcknowledged = 0
-            THEN 'Y' ELSE 'N'
+            THEN 'true' ELSE 'false'
         END AS Stage5b
-
     FROM ASData_PL.Acc_User u
     LEFT JOIN ASData_PL.Acc_AccountUserRole aur ON u.Id = aur.UserId
     LEFT JOIN ASData_PL.Acc_Account a ON aur.AccountId = a.Id
@@ -68,11 +65,21 @@ cte_Stages AS (
     LEFT JOIN cte_Agreement ag ON a.Id = ag.AccountId
     WHERE a.Name <> 'MY ACCOUNT'
 )
+-- sonar-ignore-end
 
 SELECT
-    EmployerAccountId,
-    MAX(EmployerName) AS EmployerName,
     UserEmail,
+
+    COUNT(DISTINCT EmployerAccountId) AS AccountCount,
+
+    -- Optional: expose account only if unambiguous 
+    CASE
+        WHEN COUNT(DISTINCT EmployerAccountId) = 1
+            THEN MAX(EmployerAccountId)
+        ELSE NULL
+    END AS EmployerAccountId,
+
+    MAX(EmployerName) AS EmployerName,
 
     CASE WHEN MIN(Stage1a) = MAX(Stage1a) THEN MIN(Stage1a) ELSE '' END AS Stage1a,
     CASE WHEN MIN(Stage1b) = MAX(Stage1b) THEN MIN(Stage1b) ELSE '' END AS Stage1b,
@@ -85,11 +92,6 @@ SELECT
 
 FROM cte_Stages
 GROUP BY
-    UserEmail,
-    EmployerAccountId
---ORDER BY
---    UserEmail,
---    EmployerAccountId;
-GO
+    UserEmail;
 
 
